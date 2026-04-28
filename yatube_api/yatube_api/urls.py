ec258e8b909d9c8320145b00f8a4746d1b621cc4
Artem <artem_888777@mail.ru>
2026-04-28 15:55:47 +0500

    fix: PEP8

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
-from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
